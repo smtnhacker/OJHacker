f1e5b013b8fa2a38e4164bd8516bc7cc66e85ba2
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2021-04-03 21:16:46 +0800

    DOCS: Add documentation for TCGenerator class

--- src/generators/__init__.py
+++ src/generators/__init__.py
@@ -4,17 +4,29 @@ def none():
     return "No function available"
 
 class TCGenerator:
+    '''
+        Test case generator class that facilitates creation of test case generators for every problem
+
+    '''
     __generators = {
+        # Every [TYPE] maps to an array of generator functions where index is equal to [NUM]
+        # Use placeholder function `none` for cases where the generator function is not yet available
         "PA":[
-            none, # Placeholder so index == PA #
+            none,
             none,
             none,
             none,
             pa04,
         ]
+
+        "MP":[]
+
+        "LE":[]
     }
     def __init__(self, solutions):
         self.__checkers = {
+            # Every [TYPE] maps to an array of checker functions where index is equal to [NUM]
+            # Use placeholder function `none` for cases where the checker function is not yet available
             "PA":[
                 none, # Placeholder
                 none,
